de3a04068eff506318d454f771f6921b005fa95e
Yofa08 <yofa521314@gmail.com>
2023-04-15 22:22:14 +0800

    Initial commit

---
+++ app/constant.ts
@@ -0,0 +1,8 @@
+export const OWNER = "GOOGLE";
+export const REPO = "GOOGLE";
+export const REPO_URL = `https://google.com`;
+export const ISSUE_URL = `https://google.com`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `hhttps://google.com`;
+export const FETCH_TAG_URL = `https://google.com`;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
